49bc463cc39b3be6c3710c63e50ba3b243abc0a7
Raniel-Athayde <144069898+Raniel-Athayde@users.noreply.github.com>
2023-11-29 18:43:19 -0300

    Create stm.py

---
+++ stm.py
@@ -0,0 +1,25 @@
+class time():
+
+class jogador():
+    def __init__(self, nome, time, estado_de_saude, desempenho)
+            self.nome = nome
+            self.time = time 
+            self.saude = estado de saude
+
+
+
+print("bem vindo ao TMA")   
+print('''selecione a opção:
+1 - verificar jogadores
+2 - adicionar jogador
+'''
+
+escolha = input
+
+if(escolha == 1)
+
+
+elif(escolha == 2)
+    
+else 
+    print("opção invalida")
