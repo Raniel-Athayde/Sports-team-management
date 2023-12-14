e70f585bfaa9a2d19f1107134f6a621cfa5a1fdd
Raniel-Athayde <144069898+Raniel-Athayde@users.noreply.github.com>
2023-12-13 22:12:13 -0300

    Update stm.py

--- stm.py
+++ stm.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import platform
+from traceback import print_tb
 
 def limpar_terminal():
     sistema = platform.system()
@@ -10,80 +11,238 @@ def limpar_terminal():
         os.system('clear')
 
 class Jogador:
-    def __init__(self, nome, time, estado, desempenho):
+    def __init__(self, nome, ocorrencias, desempenho, posicao):
         self.nome = nome
-        self.time = time
-        self.saude = estado
+        self.ocorrencias = ocorrencias
         self.desempenho = desempenho
+        self.posicao = posicao
 
     def mudar_time(self, time):
         self.time = time
 
+    def mudar_nome(self, nome):
+        self.nome = nome
+
 def novo_jogador(lista_jogadores):
     nome = input("\nNome do jogador: ")
-    time = input("Time do jogador: ")
-    njogador = Jogador(nome, time, "saudavel", 0)
+    ocorrencias = []
+    deesempenho = []
+    estado = True
+    njogador = Jogador(nome, estado, ocorrencias, deesempenho, "indefinida")
     lista_jogadores.append(njogador)
 
-def verificar_jogadores(lista_jogadores):
-    for i, jogador in enumerate(lista_jogadores, start=1):
-        print(f"{i} - {jogador.nome}")
+class Time:
+    def __init__(self, nome, jogadores, prox_jogos, vict, derr, em):
+        self.nome = nome
+        self.jogadores = jogadores
+        self.prox_jogos = prox_jogos
+        self.vict = vict
+        self.derr = derr
+        self.em = em
+
+    def novo_nome(self, nome):
+        self.nome = nome
+
+def listar(lista):
+    for i, elemento in enumerate(lista, start = 1):
+        print(f"    {i} - {elemento.nome}")
     print("\n")
 
-def salvar_jogadores(lista_jogadores):
-    with open('dados_jogadores.pkl', 'wb') as file:
-        pickle.dump(lista_jogadores, file)
+def listar_jogos(lista):
+    for i, elemento in enumerate(lista, start = 1):
+        print(f"    {i} - {elemento}")
+    print("\n")
 
-def carregar_jogadores():
+def remover(lista, item):
+    print("Qual" + item + "você deseja remover?")
+    i_item = input("Escreva o numero do time: ")
+    del lista[int(i_item) - 1]
+
+def novo_time(lista_times):
+    jogadores = []
+    prox_jogos = []
+    nome = input("Digite o nome do time: ")
+    ntime = Time(nome, jogadores, prox_jogos, 0, 0, 0)
+    lista_times.append(ntime)
+
+def salvar_times(lista_times):
+    with open('dados_times.pkl', 'wb') as file:
+        pickle.dump(lista_times, file)
+
+def carregar_times():
     try:
-        with open('dados_jogadores.pkl', 'rb') as file:
+        with open('dados_times.pkl', 'rb') as file:
             return pickle.load(file)
     except FileNotFoundError:
         return []
+    
+def vizualizar_jogador(jogador):
+    print(jogador.nome)
+
+
+    
+def vizualizar_time(time, lista):
+    while True:
+        limpar_terminal()
+        print("Time: " + time.nome)
+        listar(time.jogadores)
+        print("\n")
+        print("Selecione a opção:")
+        print("1 - Adicionar Jogador")
+        print("2 - Vizualizar jogador")
+        print("3 - Remover jogador")
+        print("4 - vizualizar jogos")
+        print("5 - vizualizar resultados")
+        print("6 - Editar nome")
+        print("7 - voltar")
+        print("\n")
+        escolha = input("Sua opção: ")
+
+        if escolha == "1":
+            novo_jogador(time.jogadores)
+
+        if escolha == "2":
+            ijogador = input("Insira o numero do jogador: ")
+            vizualizar_jogador(time.jogadores[int(ijogador) - 1])
+
+        elif escolha == "3":
+            remover(time.jogadores, "jogador")
+
+        elif escolha == "4":
+            limpar_terminal()
+            print("Time: " + time.nome)
+            print("Seus jogos:\n")
+            listar_jogos(time.prox_jogos)
+            print("O que deseja fazer?")
+            print("1 - Adicionar jogo")
+            print("2 - Concluir jogo")
+            print("3 - Voltar")
+
+            escolha2 = input("Sua opão: ")
+
+            if escolha2 == "1":
+                evento = input("Insira aqui o nome do evento: ")
+                time.prox_jogos.append(evento)
+
+            elif escolha2 == "2":
+                ievento = input("Escolha o evento que deseja concluir: ")
+                del time.prox_jogos[int(ievento) - 1]
+                print("Qual foi o resultado do jogo?")
+                print("1 - Vitória")
+                print("2 - Derrota")
+                print("3 - Empate")
+                print("4 - Irrelevante")
+                resultado = input("Resultado: ")
+
+                if resultado == "1":
+                    time.vict += 1
+
+                elif resultado == "2":
+                    time.derr += 1
+                
+                elif resultado == "3":
+                    time.em += 1
+        elif escolha == "5":
+            print("n° de vitorias: " + str(time.vict)) 
+            print(" n° de derrotas: " + str(time.derr))
+            print(" n° de empates: " + str(time.em))
+            total = time.vict + time.derr + time.em
+            porcetagem = (time.vict / total) * 100
+            print("resultado: " + str(time.vict) +"/" + str(total) + "(" +  str(porcetagem) + "%)\n")
+            input("precione enter para continuar")
+
+        elif escolha == "6":
+            nome = input("Digite o novo nome: ")
+            time.novo_nome(nome)
+
+        elif escolha == "7":
+            break
+
+        else:
+            print("Opção invalida")
+            input("Aperte ENTER para continuar")
+        
+        salvar_times(lista)
+
+
+'''           
+def editar_jogador(lista_jogadores):
+    limpar_terminal()
+    print("Qual jogador deseja editar?")
+    listar(lista_jogadores)
+    i_jogador = input("Digite o número correspondente ao jogador:")
+    print("O que deseja fazer?")
+    print("1 - Mudar o nome")
+    print("2 - Adicionar ocorrencia")
+    print("3 - Deletar")
+    print("4 - Voltar")
+    acao = input("Sua opção: ")
+    
+    if acao == 1:
+        print("Nome atual:" + lista_jogadores[int(i_jogador) - 1].nome)
+        novo_nome = input("Escreva o novo nome: ")
+        lista_jogadores[int(i_jogador - 1)].mudar_nome(novo_nome)
+
+    elif acao == 2:
+        print("ainda não implementado")
+        input("Aperte ENTER para continuar")
+
+    elif acao == 3:
+        print("Tem certeza que deseja deletar esse jogador?")
+        print("1 - Sim")
+        print("2 - Não")
+        confirm = input("Sua opção: ")
+
+    elif acao == 4:
+        print("ainda não implementado")
+        input("Aperte ENTER para continuar")        
+    
+    else:
+        print("Opção invalida")
+        input("aperte ENTER para continuar")
+        
+    editar_jogador(lista_jogadores)
+'''
 
 def inicio():
-    listaj = carregar_jogadores()
+    listat = carregar_times()
 
     while True:
         limpar_terminal()
         print("\nBem-vindo ao STM\n")
         print("Selecione a opção:")
-        print("1 - Verificar jogadores")
-        print("2 - Adicionar jogador")
+        print("1 - Verificar times")
+        print("2 - Adicionar time")
         print("3 - Salvar e encerrar")
 
         escolha = input("Sua opção: ")
 
         if escolha == "1":
             limpar_terminal()
-            print("\nSeus jogadore salvos são:\n")
-            verificar_jogadores(listaj)
+            print("\nSeus times salvos são:\n")
+            listar(listat)
             print("Secione a opção:\n")
-            print("1 - Voltar\n")
-            print("2 - Editar jogador\n")
+            print("1 - Vizualizar time\n")
+            print("2 - Voltar\n")
             escolha2 = input("Sua opção: ")
-            if escolha2 == 1:
-                inicio()
-            elif escolha2 == 2:
-                editar()
+            if escolha2 == "1":
+                print("Qual time deseja vizualizar?")
+                escolha3 = input("Escreva o número do time: ")
+                vizualizar_time(listat[int(escolha3) - 1], listat)
+            elif escolha2 == "2":
+                pass
             else:
                 print("Opção inválida")
 
         elif escolha == "2":
-            novo_jogador(listaj)
-            salvar_jogadores(listaj)
+            novo_time(listat)
+            salvar_times(listat)
         elif escolha == "3":
-            salvar_jogadores(listaj)
+            salvar_times(listat)
             break
         else:
             print("Opção inválida")
-
-def editar():
-    limpar_terminal()
-    print("Qual jogador deseja editar?")
-    verificar_jogadores(listaj)
-    input("Digite o número correspondente ao jogador:")
-    inicio()
+            input("Aperte ENTER para continuar")
 
 if __name__ == "__main__":
     inicio()
